fb1ded24337ceef6db1218ba4cf70a06f6053fea
SirDuck999 <147013104+SirDuck999@users.noreply.github.com>
2024-04-14 21:45:02 -0700

    Update ControlsBoxWirelessV2.ino

--- ControlsBoxWirelessV2.ino
+++ ControlsBoxWirelessV2.ino
@@ -13,46 +13,40 @@ const int mqtt_port = 1883;
 
 // MQTT topics
 const char* switch_topic = "esp32/switch";
-const char* ac_topic = "esp32/ac";
+const char* data_topic = "esp32/data";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-#define RO_PIN 16
-#define DI_PIN 17
-#define outlet 2 //32
-//#define SER_BUF_SIZE 1024
-#define fill 22
-#define dump 21 
-#define vent 19
-#define qd 18
-#define purge 5
-#define mpv 4
-#define ignite 15
-#define abortSiren 23
-#define abortValve 13
+#define RO_PIN 16 //32 boRD
+#define DI_PIN 17 //35
+//jumper wire to HIGH
 
-#define fillAc 34
-#define dumpAc 35
-#define ventAc 32
-#define qdAc 33
-#define purgeAc 25
-#define mpvAc 26
-#define abortSirenAc 27
-#define igniteAc 14
+#define RE 26 //13
+#define DE 27
+//RE to 26
+//DE to 27
 
-int state;
+#define purge 22 //22
+#define fill 23 //23
+#define abortValve 21 //21
+#define dump 19 //19
+#define vent 18 //18
+#define qd 4 //4 // always on
+#define ignite 2 //2
+#define mpv 15 //15
+#define heatpad 5 //17
+#define siren 13 //22
+#define sirenPower 32 //23
+
+unsigned long long delay_time = 500;
+unsigned long long last_time = 0;
 String message = "";
-String actuation = "";
-
-unsigned long long delay_time = 250;
-unsigned long long last_time = 0;  
-unsigned long long last_time_send = 0;  
+String data = "";
 
 //HardwareSerial rs485Serial(2);
 
 void setup_wifi() {
-  delay(10);
   // Connect to Wi-Fi network with SSID and password
   Serial.println();
   Serial.print("Connecting to ");
@@ -71,23 +65,19 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {    
+  //Serial.print("Message arrived [");
+  //Serial.print(topic);
+  //Serial.print("] ");
+  for (int i = 0; i < length; i++) {
     if ((i == 0 && (char)payload[i] != 'A') || (i == length - 1 && (char)payload[i] != 'Z')){
         return;
     }
 
     if (i > 0 && i < length - 1){
-      if ((char)payload[i] != '0' && (char)payload[i] != '1'){
-        Serial.println("Message: invalid message");
-        return;
-      }
-      message += (char)payload[i];
+      data += (char)payload[i];
     }
   }
-  Serial.println("Message: " + message);
+  Serial.println(data);
 }
 
 void reconnect() {
@@ -95,16 +85,14 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP32ClientControls"/*, mqtt_username, mqtt_password*/)) {
+    if (client.connect("ESP32Client"/*, mqtt_username, mqtt_password)*/)) {
       Serial.println("connected");
-      // Once connected, subscribe to the topic
-      client.subscribe(switch_topic);
+      client.subscribe(data_topic, 0);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+
     }
   }
 }
@@ -112,171 +100,76 @@ void reconnect() {
 void setup() {
   Serial.begin(115200);
   setup_wifi();
-  client.setServer(mqtt_server, mqtt_port);
+  client.setServer(mqtt_server, mqtt_port);\
   client.setCallback(callback);
 
-  //Define all controls
-//  pinMode(spare, OUTPUT);
-  pinMode(abortSiren, OUTPUT);
-  pinMode(ignite, OUTPUT);
-  pinMode(fill, OUTPUT);
-  pinMode(vent, OUTPUT);
-  pinMode(dump, OUTPUT);
-  pinMode(qd, OUTPUT);
-  pinMode(mpv, OUTPUT);  
-  pinMode(purge, OUTPUT);   
-  pinMode(outlet, OUTPUT);
-  pinMode(abortValve, OUTPUT);   
 
-  digitalWrite(abortSiren, LOW);//off
-  digitalWrite(ignite, LOW);//off
-  digitalWrite(fill, HIGH);//closed
-  digitalWrite(vent, HIGH);//open
-  digitalWrite(dump, HIGH);//open
-  digitalWrite(qd, HIGH);//open
-  digitalWrite(mpv, HIGH);
-  digitalWrite(purge, HIGH);//closed
-  digitalWrite(outlet, HIGH);//closed
-  digitalWrite(abortValve, LOW);//closed
+  pinMode(RE, OUTPUT);
+  pinMode(DE, OUTPUT);
+  digitalWrite(RE, HIGH);
+  digitalWrite(DE, HIGH); 
+
+  pinMode(purge, INPUT_PULLDOWN);
+  pinMode(fill, INPUT_PULLDOWN);
+  pinMode(abortValve, INPUT_PULLDOWN);
+  pinMode(dump, INPUT_PULLDOWN);
+  pinMode(vent, INPUT_PULLDOWN);
+  pinMode(qd, INPUT_PULLDOWN);
+  pinMode(ignite, INPUT_PULLDOWN);
+  pinMode(mpv, INPUT_PULLDOWN);
+  pinMode(siren, INPUT_PULLDOWN);
+  pinMode(sirenPower, OUTPUT);
+
+  digitalWrite(sirenPower, HIGH);
   
-  pinMode(purgeAc, INPUT_PULLDOWN);
-  pinMode(fillAc, INPUT_PULLDOWN);
-  pinMode(abortSirenAc, INPUT_PULLDOWN);
-  pinMode(dumpAc, INPUT_PULLDOWN);
-  pinMode(ventAc, INPUT_PULLDOWN);
-  pinMode(qdAc, INPUT_PULLDOWN);
-  pinMode(igniteAc, INPUT_PULLDOWN);
-  pinMode(mpvAc, INPUT_PULLDOWN);
+
+  Serial.println("Setup Complete");
 
   last_time = millis();
-  last_time_send = millis();
 }
 
 void loop() {
   if (!client.connected()) {
     reconnect();
   }
+
   client.loop();
 
-  while (millis() - last_time > 3000){
-    Serial.println("Unavailable...");
-    if (millis() - last_time > 10000){
-      digitalWrite(abortSiren, HIGH);
-    }
-    if (millis() - last_time > 20000){
-      Serial.println("Aborted...");
-      digitalWrite(ignite, LOW);
-      digitalWrite(fill, HIGH);
-      digitalWrite(vent, HIGH);
-      digitalWrite(dump, HIGH);
-      digitalWrite(qd, HIGH);
-      digitalWrite(mpv, HIGH);
-      digitalWrite(purge, HIGH);
-      digitalWrite(ignite, LOW);
-      digitalWrite(abortValve, LOW);
-    }
-  }
-  
+  unsigned long currentMillis = millis();
+  // Every X number of seconds 
+  // it publishes a new MQTT message
+   if((millis() - last_time) > delay_time)
+  {
+    String Purge = String(digitalRead(purge));
+    String Fill = String(digitalRead(fill));
+    String AbortValve = String(digitalRead(abortValve));
+    String Dump = String(digitalRead(dump));
+    String Vent = String(digitalRead(vent));
+    String QD = String (digitalRead(qd));
+    String Ignite = String(digitalRead(ignite));
+    String MPV = String(digitalRead(mpv));
+    String Heatpad = String(digitalRead(heatpad));
+    String Siren = String(digitalRead(siren));
 
-  if (message != ""){
-    const char ACTUATED = '1';
+    message = ('A' + AbortValve + QD + Vent + Ignite + Purge + Fill + Dump + Heatpad + MPV + Siren + 'Z');
 
-    const short PURGE_SWITCH = 4;
-    const short FILL_SWITCH = 5;
-    const short ABORT_SIREN_SWITCH = 9;
-    const short DUMP_SWITCH = 6; 
-    const short VENT_SWITCH = 2;
-    const short QD_SWITCH = 1;
-    const short IGNITE_SWITCH = 3;
-    const short MPV_SWITCH = 8;
-    const short OUTLET_SWITCH = 7;
-    const short ABORT_VALVE_SWITCH = 0;
-
-    if(message[FILL_SWITCH] == ACTUATED)
-      digitalWrite(fill, LOW);
-    if(message[FILL_SWITCH] == '0')
-      digitalWrite(fill, HIGH);
-    
-    if(message[ABORT_SIREN_SWITCH] == ACTUATED)
-      digitalWrite(abortSiren, HIGH);
-    if(message[ABORT_SIREN_SWITCH] == '0')
-      digitalWrite(abortSiren, LOW);
-    
-    if(message[DUMP_SWITCH] == ACTUATED)
-      digitalWrite(dump,LOW);
-    if(message[DUMP_SWITCH] == '0')
-      digitalWrite(dump, HIGH);
-      
-    if(message[VENT_SWITCH] == ACTUATED)
-      digitalWrite(vent,LOW);
-    if(message[VENT_SWITCH] == '0')
-      digitalWrite(vent, HIGH);
-  
-    if(message[PURGE_SWITCH] == ACTUATED)
-      digitalWrite(purge, LOW);
-    if(message[PURGE_SWITCH] == '0')
-      digitalWrite(purge, HIGH);
-
-    if(message[QD_SWITCH] == ACTUATED)
-      digitalWrite(qd,LOW);
-    if(message[QD_SWITCH] == '0')
-      digitalWrite(qd, HIGH);
-
-    if(message[IGNITE_SWITCH] == ACTUATED)
-      digitalWrite(ignite, HIGH);
-    if(message[IGNITE_SWITCH] == '0')
-      digitalWrite(ignite, LOW);
-
-    if(message[MPV_SWITCH] == ACTUATED)
-      digitalWrite(mpv, LOW);
-    if(message[MPV_SWITCH] == '0')
-      digitalWrite(mpv, HIGH);
-
-    if(message[OUTLET_SWITCH] == ACTUATED)
-      digitalWrite(outlet, LOW);
-    if(message[OUTLET_SWITCH] == '0')
-    digitalWrite(outlet, HIGH);
-
-    if(message[ABORT_VALVE_SWITCH] == ACTUATED)
-      digitalWrite(abortValve, HIGH);
-    if(message[ABORT_VALVE_SWITCH] == '0')
-    digitalWrite(abortValve, LOW);
-    
-    last_time = millis();
-
-    message = "";
-  }
-  
-  if (millis() - last_time_send > delay_time) {
-    String abortSirenStr = String(digitalRead(abortSirenAc));
-    String igniteStr = String(digitalRead(igniteAc));
-    String fillStr = String(digitalRead(fillAc));
-    String ventStr = String(digitalRead(ventAc));
-    String dumpStr = String(digitalRead(dumpAc));
-    String qdStr = String(digitalRead(qdAc));
-    String mpvStr = String(digitalRead(mpvAc));
-    String purgeStr = String(digitalRead(purgeAc));
-
-    actuation = "A" + abortSirenStr + 
-                " " + igniteStr + 
-                " " + fillStr + 
-                " " + ventStr + 
-                " " + dumpStr +
-                " " + qdStr +
-                " " + mpvStr +
-                " " + purgeStr + "Z";
-    
-    // Check if the message length exceeds the maximum packet size
-    /*if (actuation.length() <= MQTT_MAX_PACKET_SIZE) {
-        // Publish the message if it's within the size limit
-        client.publish(ac_topic, actuation.c_str());
-        Serial.println("Message sent: " + actuation);
+    // Check message length before publishing
+    if (message.length() <= MQTT_MAX_PACKET_SIZE) {
+      client.publish(switch_topic, message.c_str());
+      //Serial.println("Sent:");
+      //Serial.println(message);
     } else {
-        // Handle the case where the message is too long
-        Serial.println("Message too long to publish.");
-    }*/
-    actuation = "";
+      //Serial.println("Message too long to publish.");
+    }
 
-    last_time_send = millis();
-  }    
+    message = ""; // Clear the message variable
+
+    last_time = millis();
+  }
+
+  if (data != ""){
+    //Serial.println("Actuation: ");
+    //Serial.println(actuation);
+    data = "";
+  }
 }
