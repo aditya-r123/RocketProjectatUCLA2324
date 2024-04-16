30e90d68591cf467e32b2c5c0923fa2ceac5edcf
Aditya Rao <54918123+aditya-r123@users.noreply.github.com>
2024-04-15 19:12:47 -0700

    Update graf_test.py

--- graf_test.py
+++ graf_test.py
@@ -2,31 +2,40 @@ from serial import Serial
 import socket
 import time
 import re
+import os
+import random
 
-#MODIFY PORT AND BAUDRATE 
-#PORT = '/dev/cu.usbserial-0001'
-PORT = '/dev/cu.SLAB_USBtoUART'
-#PORT = '/dev/cu.usbmodem131488301'
-BAUDRATE = 9600
+# MODIFY PORT AND BAUDRATE
+# PORT = '/dev/cu.usbserial-0001' #--> UNCOMMENT THIS
+#PORT = '/dev/cu.SLAB_USBtoUART'
+# PORT = '/dev/cu.usbmodem131488301'
+BAUDRATE = 115200
+MAX_COUNT = 100 #######Change this
 
 measurement = 'sensorvals'
 
-field_keys = ["pt1", "pt2", "pt3", "pt4", "pt5", "pt6", "lc1", "lc2", "tc1", "tc2"]
+field_keys = ["pt1", "pt2", "pt3", "pt4", "pt5", "pt6", "lc1", "lc2"]
 
-
-#just in case
+# just in case
 def getTime():
     return time.time_ns()
+
 start_time = str(getTime())
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-#DO I NEED TO CHANGE SERVER ADDRESS PORT????
-serverAddressPort = ('127.0.0.1', 4000)
-ser = Serial(PORT, BAUDRATE, timeout=0.1)
+
+# DO I NEED TO CHANGE SERVER ADDRESS PORT????
+serverAddressPort = ('127.0.0.1', 4001)
+#ser = Serial(PORT, BAUDRATE, timeout=0.1)
+
+# Modify value of N for varying smoothness
+N = 3
+packet_counter = 0
+
 while True:
     try:
         print('ser\n')
         print(ser)
-        ser = Serial(PORT, BAUDRATE, timeout=0.1)
+        #ser = Serial(PORT, BAUDRATE, timeout=0.1)
         print('ser\n')
         print(ser)
         print('ser\n')
@@ -36,41 +45,49 @@ while True:
         print('Successfully connected to Serial device')
         break
 
-#Modify value of N for varying smoothness
-N = 3
-
+time.sleep(3)
 while True:
-    for i in range(N):
-        timestamp = str(getTime())
-        print(ser.readline())
-        line = ser.readline().decode()
-        print(line)
-        #line = "0.1233 PT1: 100, PT2: 200, PT3: 300, PT4: 400, PT5: 500, PT6: 600, LC1: 0.1, LC2: 0.2, TC1: 1, TC2: 2"
+    timestamp = str(getTime())
+    #line2 = ser.readline() #--> UNCOMMENT THIS
+    packet_counter += 1
 
+    if packet_counter == MAX_COUNT:
+        #line = line2.decode() #--> UNCOMMENT THIS
+        
+        #line = "0.1233 PT1: 100, PT2: 200, PT3: 3000, PT4: 400, PT5: 500, PT6: 60, LC1: 0.1, LC2: 0.9" #--> COMMENT THIS
+        #line = "0.1233 PT1: 100, PT2: 200, PT3: 3000, PT4: 400, PT5: 500, PT6: 60, LC1: 0.1, LC2: " #--> COMMENT THIS
+        #line+=str(random.randint(0, 3))
+        # Original string
+        line = "0.1233 PT1: 100, PT2: 200, PT3: 3000, PT4: 400, PT5: 500, PT6: 60, LC1: 0.1, LC2: 0.9"
+
+    
+            
+        print(line)
         line = line.replace(",", "")
-        #this starts the string at the first PT
-        #the millis value that prints should be ignored
-        index_of_p = line.find('P') 
+        # this starts the string at the first PT
+        # the millis value that prints should be ignored
+        index_of_p = line.find('P')
         line = line[index_of_p:]
         # Processing data
         line_processing = re.sub(r'[A-Z]+\d:', ',', line)
         line_processing = line_processing.replace(",", "", 1)
         line_processing = "".join(line_processing.split())
         data = line_processing.split(',')
+        # Reset packet counter
+        packet_counter = 0
 
+        # Sending data to Grafana as:
+        # pt1,pt2,pt3,pt4,pt5,pt6,lc1,lc2,tc1,tc2
 
-
-        #Sending data to Grafana as:
-        #pt1,pt2,pt3,pt4,pt5,pt6,lc1,lc2,tc1,tc2
-        
-        #combine all values together, seperated by comma
+        # combine all values together, separated by comma
         fields = ''
-        for key,val in zip(field_keys, data):
+        for key, val in zip(field_keys, data):
             fields += f'{key}={val},'
-        #delete comma after tc2
+        # delete comma after tc2
         fields = fields.strip(',')
 
         # create influx string
         influx_string = measurement + ' ' + fields + ' ' + timestamp
-        #print(influx_string)
+        print(influx_string)
         UDPClientSocket.sendto(influx_string.encode(), serverAddressPort)
+        time.sleep(0.5)
