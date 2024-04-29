3a10e6dd73a1c75443ddda3b3c2474c57ca5f635
SirDuck999 <147013104+SirDuck999@users.noreply.github.com>
2024-04-29 16:46:24 -0700

    Update Switchbox.ino

--- ground-sys/Switchbox.ino
+++ ground-sys/Switchbox.ino
@@ -24,18 +24,19 @@ rs485Serial.begin(115200, EspSoftwareSerial::SWSERIAL_8N1, RX, TX)
 
 #define purge 22 //22
 #define fill 23 //23
-#define abortSiren 21 //21
+#define abortValve 21 //21
 #define dump 19 //19
 #define vent 18 //18
 #define qd 4 //4 // always on
 #define ignite 2 //2
 #define mpv 15 //15
-#define abort 5 //17
+#define heatpad 5 //17
+#define siren 13 //22
+#define sirenPower 32 //23
 
 unsigned long long delay_time = 250;
 unsigned long long last_time = 0;
 
-String oldMessage = "AZ";
 
 HardwareSerial rs485Serial(2);
 
@@ -49,12 +50,16 @@ void setup() {
 
   pinMode(purge, INPUT_PULLDOWN);
   pinMode(fill, INPUT_PULLDOWN);
-  pinMode(abortSiren, INPUT_PULLDOWN);
+  pinMode(abortValve, INPUT_PULLDOWN);
   pinMode(dump, INPUT_PULLDOWN);
   pinMode(vent, INPUT_PULLDOWN);
   pinMode(qd, INPUT_PULLDOWN);
   pinMode(ignite, INPUT_PULLDOWN);
   pinMode(mpv, INPUT_PULLDOWN);
+  pinMode(siren, INPUT_PULLDOWN);
+  pinMode(sirenPower, OUTPUT);
+
+  digitalWrite(sirenPower, HIGH);
   
   Serial.begin(115200);
   rs485Serial.begin(115200, SERIAL_8N1, RO_PIN, DI_PIN);
@@ -72,15 +77,16 @@ void loop() {
 
     String Purge = String(digitalRead(purge));
     String Fill = String(digitalRead(fill));
-    String AbortSiren = String(digitalRead(abortSiren));
+    String AbortValve = String(digitalRead(abortValve));
     String Dump = String(digitalRead(dump));
     String Vent = String(digitalRead(vent));
     String QD = String (digitalRead(qd));
     String Ignite = String(digitalRead(ignite));
     String MPV = String(digitalRead(mpv));
-    String Abort = String(digitalRead(abort));
+    String Heatpad = String(digitalRead(heatpad));
+    String Siren = String(digitalRead(siren));
 
-    message = ('A' + AbortSiren + QD + Vent + Ignite + Purge + Fill + Dump + Abort + MPV + 'Z');
+    message = ('A' + AbortValve + QD + Vent + Ignite + Purge + Fill + Dump + Heatpad + MPV + Siren + 'Z');
     
     rs485Serial.println(message);
     Serial.println("Sent:");
