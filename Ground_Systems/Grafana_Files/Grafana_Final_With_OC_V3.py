1e7f9963bff0b58297337c5ccc4ef7e30fe21b7d
Aditya Rao <54918123+aditya-r123@users.noreply.github.com>
2024-05-17 23:45:37 -0700

    Update Grafana_Final_With_OC_V3.py

--- Ground_Systems/Grafana_Files/Grafana_Final_With_OC_V3.py
+++ Ground_Systems/Grafana_Files/Grafana_Final_With_OC_V3.py
@@ -54,75 +54,38 @@ while True:
 
     if packet_counter != MAX_COUNT:
         #line = line2.decode() #--> UNCOMMENT THIS
-        line = "0.1233 PT1:100, PT2:200, PT3:300, PT4:400, PT5:900, PT6:600, LC1:100, LC2:200, S:0, I:0, F:0, V:0, D:0, Q:1, M:0, P:0" #--> COMMENT THIS
-        line = line.replace(", S", "|S")
+        line = "100, 200, 300, 400, 900, 600, 20, 200, 10100100"
+        Line = line[:-1]
+        line = line.strip().replace(" ", "")  # Clean the input line
+        # Split the input line into individual values
+        data = line.split(',')
+        opto_dataa = ", ".join(data[-1])
+        print(opto_dataa)
+        opto_data = [int(x.strip()) for x in opto_dataa.split(',')]
+        sensor_data = data[:-1]
+        print(sensor_data)
 
-
-    
-        '''    
-
-        line = line.replace(",", "")
-        # this starts the string at the first PT
-        # the millis value that prints should be ignored
-        index_of_p = line.find('P')
-        line = line[index_of_p:]
-        
-        # Processing data
-        line_processing = re.sub(r'[A-Z]+\d:', ',', line)
-        line_processing = line_processing.replace(",", "", 1)
-        line_processing = "".join(line_processing.split())
-        data = line_processing.split(',')
-        '''
-        line = line.replace(",", "")
-        print("LINE IS " + line)
-        # Find the index of 'PT'
-        index_of_p = line.find('P')
-        line = line[index_of_p:]
-
-        # Find the index of '|', and if not found, set it to the end of the line
-        index_of_pipe = line.find('|')
-        if index_of_pipe == -1:
-           index_of_pipe = len(line)
-
-        # Processing data until '|'
-        line_processing = re.sub(r'[A-Z]+\d:', ',', line[:index_of_pipe-1])
-        line_processing = line_processing.replace(",", "", 1)
-        line_processing = "".join(line_processing.split())
-        data = line_processing.split(',')
-        print(data)
-        
-        index = line.find("|")
-        opto_part = line[index_of_pipe+1:]  # Get everything after '|'
-        opto_data = re.findall(r'\d+', opto_part)  # Find all numeric values using regex
-        
         # Reset packet counter
         packet_counter = 0
 
-        # Sending data to Grafana as:
-        # pt1,pt2,pt3,pt4,pt5,pt6,lc1,lc2,tc1,tc2
-
-        # combine all values together, separated by comma
+        # Prepare and send data for sensor values
         fields = ''
-        for key, val in zip(field_keys, data):
+        for key, val in zip(field_keys, sensor_data):
             fields += f'{key}={val},'
-        # delete comma after tc2
         fields = fields.strip(',')
 
-        # create influx string
         influx_string = measurement + ' ' + fields + ' ' + timestamp
         print(influx_string)
         UDPClientSocket.sendto(influx_string.encode(), serverAddressPort)
 
-        # combine all values together, separated by comma
+        # Prepare and send data for opto values
         fields2 = ''
         for key2, val2 in zip(field_keys2, opto_data):
             fields2 += f'{key2}={val2},'
-        # delete comma after last
         fields2 = fields2.strip(',')
 
-        # create influx string
         influx_string2 = measurement2 + ' ' + fields2 + ' ' + timestamp
         print(influx_string2)
         UDPClientSocket.sendto(influx_string2.encode(), serverAddressPort2)
 
-        time.sleep(0.24) #--> COMMENT THIS
+        time.sleep(0.24)
