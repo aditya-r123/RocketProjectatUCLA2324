7884aa62f7e74b81537dff2cda042ca49048b9c2
Aditya Rao <54918123+aditya-r123@users.noreply.github.com>
2024-05-18 00:02:31 -0700

    Update Grafana_Final_No_OC.py

--- Ground_Systems/Grafana_Files/Grafana_Final_No_OC.py
+++ Ground_Systems/Grafana_Files/Grafana_Final_No_OC.py
@@ -1,4 +1,4 @@
-from serial import Serial
+#from serial import Serial
 import socket
 import time
 import re
@@ -12,7 +12,7 @@ PORT = '/dev/cu.SLAB_USBtoUART' #--> UNCOMMENT THIS
 BAUDRATE = 115200
 MAX_COUNT = -31 #######Change this
 
-measurement = 'sensorvals2'
+measurement = 'sensorvals'
 
 field_keys = ["pt1", "pt2", "pt3", "pt4", "pt5", "pt6", "lc1", "lc2"]
 
@@ -25,7 +25,7 @@ UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 # DO I NEED TO CHANGE SERVER ADDRESS PORT????
 serverAddressPort = ('127.0.0.1', 4001)
-ser = Serial(PORT, BAUDRATE, timeout=0.1)
+#ser = Serial(PORT, BAUDRATE, timeout=0.1)
 
 # Modify value of N for varying smoothness
 N = 3
@@ -47,15 +47,15 @@ while True:
 
 while True:
     timestamp = str(getTime())
-    line2 = ser.readline() #--> UNCOMMENT THIS
+    #line2 = ser.readline() #--> UNCOMMENT THIS
     #packet_counter += 1
 
     if packet_counter != MAX_COUNT:
-        line = line2.decode() #--> UNCOMMENT THIS
+        #line = line2.decode() #--> UNCOMMENT THIS
         
         #line = "0.1233 PT1: 100, PT2: 200, PT3: 3000, PT4: 400, PT5: 500, PT6: 60, LC1: 0.1, LC2: 0.9" #--> COMMENT THIS
-        #line = "0.1233 PT1: 100, PT2: 200, PT3: 3000, PT4: 400, PT5: 500, PT6: 60, LC1: 0.1, LC2: " #--> COMMENT THIS
-        #line+=str(random.randint(0, 3))
+        line = "0.1233 PT1: 100, PT2: 200, PT3: 3000, PT4: 400, PT5: 500, PT6: 60, LC1: 0.1, LC2: " #--> COMMENT THIS
+        line+=str(random.randint(0, 3))
         # Original string
         #line = "0.1233 PT1: 10000, PT2: 2000, PT3: 3000, PT4: 4000, PT5: 9000, PT6: 6000, LC1: 0.00001, LC2: 0.009"
 
@@ -89,3 +89,4 @@ while True:
         influx_string = measurement + ' ' + fields + ' ' + timestamp
         print(influx_string)
         UDPClientSocket.sendto(influx_string.encode(), serverAddressPort)
+        time.sleep(0.05)
